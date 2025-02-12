085a5c28d6cdbd1b606d4b4e627a2f082ec008fa
ashleywashere <ashleytrades412@gmail.com>
2025-02-12 18:25:00 +0200

    Update and rename libraryy.lua to library.lua

--- library.lua
+++ library.lua
@@ -4215,9 +4215,9 @@ do
                                 --utility:LoadImage(colorpicker_open_transparency_image, "transp", "https://i.imgur.com/VcMAYjL.png")
                             end
                             --
-                            utility:LoadImage(colorpicker_open_picker_image, "valsat", "https://imgur.com/wpDRqVH")
+                            utility:LoadImage(colorpicker_open_picker_image, "valsat", "https://i.imgur.com/wpDRqVH")
                             utility:LoadImage(colorpicker_open_picker_cursor, "valsat_cursor", "https://raw.githubusercontent.com/mvonwalk/splix-assets/main/Images-cursor.png")
-                            utility:LoadImage(colorpicker_open_huepicker_image, "hue", "https://imgur.com/iEOsHFv")
+                            utility:LoadImage(colorpicker_open_huepicker_image, "hue", "https://i.imgur.com/iEOsHFv")
                             --
                             window.currentContent.frame = colorpicker_open_inline
                             window.currentContent.colorpicker = colorpicker
